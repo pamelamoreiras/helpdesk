0fa349e18adaaad80c5fa38fa7506ae707837e5e
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-12 14:01:28 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/pamela/helpdesk/service/TecnicoService.java
+++ src/main/java/com/pamela/helpdesk/service/TecnicoService.java
@@ -6,6 +6,7 @@ import com.pamela.helpdesk.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +20,7 @@ public class TecnicoService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 
+    public List<Tecnico> findAll() {
+        return repository.findAll();
+    }
 }
