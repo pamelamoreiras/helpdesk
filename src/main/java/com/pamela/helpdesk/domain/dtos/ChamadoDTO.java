b245a4ebfeebce0368a84a106311eab1a0b41e3e
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-15 11:53:50 -0300

    create para Chamado

--- src/main/java/com/pamela/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/pamela/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,7 @@ package com.pamela.helpdesk.domain.dtos;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pamela.helpdesk.domain.Chamado;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -16,11 +17,17 @@ public class ChamadoDTO implements Serializable {
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+    @NotNull(message = "O campo PRIORIDADE é requerido!")
     private Integer prioridade;
+    @NotNull(message = "O campo STATUS é requerido!")
     private Integer status;
+    @NotNull(message = "O campo TÍTULO é requerido!")
     private String titulo;
+    @NotNull(message = "O campo OBSERVAÇÕES é requerido!")
     private String observacoes;
+    @NotNull(message = "O campo TÉCNICO é requerido!")
     private Integer tecnico;
+    @NotNull(message = "O campo CLIENTE é requerido!")
     private Integer cliente;
     private String nomeTecnico;
     private String nomeCliente;
--- src/main/java/com/pamela/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/pamela/helpdesk/resources/ChamadoResources.java
@@ -5,11 +5,12 @@ import com.pamela.helpdesk.domain.dtos.ChamadoDTO;
 import com.pamela.helpdesk.service.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.swing.plaf.PanelUI;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +32,13 @@ public class ChamadoResources {
         List<Chamado> list = chamadoService.findAll();
         List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado obj = chamadoService.create(objDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
