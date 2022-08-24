c894f79a565cfa939437ec58aa10f7076559595e
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-24 15:38:29 -0300

    ajustes ao atualizar rota de  cliente service

--- src/main/java/com/pamela/helpdesk/resources/ClienteResource.java
+++ src/main/java/com/pamela/helpdesk/resources/ClienteResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/cliente")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
 
     @Autowired
