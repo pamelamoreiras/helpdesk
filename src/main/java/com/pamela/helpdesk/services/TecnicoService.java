102794f0e6e906071ef67826cd63cf8e1800c224
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-08-24 14:27:14 -0300

    ajustes ao atualizar senha de tecnico e cliente service

--- src/main/java/com/pamela/helpdesk/services/TecnicoService.java
+++ src/main/java/com/pamela/helpdesk/services/TecnicoService.java
@@ -55,6 +55,11 @@ public class TecnicoService {
     public Tecnico update(final Integer id, final TecnicoDTO objDTO) {
         objDTO.setId(id);
         Tecnico oldObj = findById(id);
+
+        if (!objDTO.getSenha().equals(oldObj.getSenha())){
+            objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+        }
+
         validaPorCpfEEmail(objDTO);
         oldObj = new Tecnico(objDTO);
         return tecnicoRepository.save(oldObj);
